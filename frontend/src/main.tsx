b3790b3f9e1422aab231fc5ea792bd5febc244ad
Mquang279 <quangm279@gmail.com>
2026-01-30 11:22:33 +0700

    feat: install package for frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './input.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage.tsx';
+import DefaultLayout from './components/layout/DefaultLayout.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+const root: HTMLElement | null = document.getElementById("root");
+
+if (!root) throw new Error("Root element not found");
+
+
+ReactDOM.createRoot(root).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <DefaultLayout>
+            <HomePage />
+          </DefaultLayout>
+        } />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
+);
